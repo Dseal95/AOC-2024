50d35fbbcd56552b8c3e12242ed632cfcb3b04d6
Dseal95 <dan.seal.06@gmail.com>
2024-12-11 13:15:30 +0000

    adding more comments.

--- day11.py
+++ day11.py
@@ -30,8 +30,9 @@ stones = "5688 62084 2 3248809 179 79 0 172169"
 # arr = expand_arr(blink(arr))
 # print(f"** part 1 = {len(arr.shape[0])} ** [time taken: {time.time() - t_start}s]")
 
-# amazing explanation of @cache decotrator: https://www.youtube.com/watch?v=pVfsmQSlVOQ&ab_channel=HyperNeutrino
-@cache
+# amazing explanation of @cache decorator: https://www.youtube.com/watch?v=pVfsmQSlVOQ&ab_channel=HyperNeutrino.
+# able to use @cache over @lru_cache as we don't need to track usage order, just result.
+@cache 
 def count(stone, steps):
     if steps == 0:
         return 1
